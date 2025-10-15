3a4418b58089e5b2b71a420562b76cbe7cee05c9
Don Syme <dsyme@github.com>
2025-10-15 16:48:01 +0100

    details for prompt

--- pkg/workflow/sh/print_prompt_summary.sh
+++ pkg/workflow/sh/print_prompt_summary.sh
@@ -1,5 +1,8 @@
-echo "## Generated Prompt" >> $GITHUB_STEP_SUMMARY
+echo "<details>" >> $GITHUB_STEP_SUMMARY
+echo "<summary>Generated Prompt</summary>" >> $GITHUB_STEP_SUMMARY
 echo "" >> $GITHUB_STEP_SUMMARY
 echo '```markdown' >> $GITHUB_STEP_SUMMARY
 cat $GITHUB_AW_PROMPT >> $GITHUB_STEP_SUMMARY
 echo '```' >> $GITHUB_STEP_SUMMARY
+echo "" >> $GITHUB_STEP_SUMMARY
+echo "</details>" >> $GITHUB_STEP_SUMMARY
