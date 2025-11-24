9a0fcfc60d97c3c7abf16e2f68b7b837942336e6
Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>
2025-11-24 22:49:02 +0000

    bump copilot CLI version
    Signed-off-by: Jiaxiao (mossaka) Zhou <duibao55328@gmail.com>

--- pkg/constants/constants_test.go
+++ pkg/constants/constants_test.go
@@ -246,7 +246,7 @@ func TestVersionConstants(t *testing.T) {
 		expected Version
 	}{
 		{"DefaultClaudeCodeVersion", DefaultClaudeCodeVersion, "2.0.50"},
-		{"DefaultCopilotVersion", DefaultCopilotVersion, "0.0.358"},
+		{"DefaultCopilotVersion", DefaultCopilotVersion, "0.0.363"},
 		{"DefaultCodexVersion", DefaultCodexVersion, "0.63.0"},
 		{"DefaultGitHubMCPServerVersion", DefaultGitHubMCPServerVersion, "v0.22.0"},
 		{"DefaultFirewallVersion", DefaultFirewallVersion, "v0.2.0"},
